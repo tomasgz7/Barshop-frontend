55602639ffb40bb04140d000801c486dfbe2e66e
tomasgz7 <tomasguzmancabj@hotmail.com>
2022-04-01 19:07:13 -0300

    Configurando las rutas.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+import  { BrowserRouter, Routes, Route } from 'react-router-dom'
+// Importamos los componentes
+import ShowProducts from './components/ShowProducts';
+import CreateProduct from './components/CreateProducts';
+import EditProduct from './components/EditProducts';
 
 function App() {
   return (
     <div className="App">
-        <button className='btn btn-primary'>
-          Crear
-        </button>
+        <BrowserRouter>
+        <Routes>
+          <Route path='' element= { <ShowProducts /> } />
+          <Route path='/create' element= { <CreateProduct /> } />
+          <Route path='edit/:id' element= { <EditProduct /> }/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
